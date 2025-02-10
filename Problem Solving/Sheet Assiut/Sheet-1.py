e5d0ef8d0288f82098c8e8c7f91ba759f20b544a
Abdullah Zaky <Abdullah.Zaky@intellisec-me.com>
2025-02-10 13:52:26 +0200

    changed the files system

--- Problem Solving/Sheet Assiut/Sheet-1.py
+++ Problem Solving/Sheet Assiut/Sheet-1.py
@@ -125,7 +125,7 @@ else: print("NOT") '''
 # ---------------------------
 
 # M. Capital or Small or Digit
-letter = input()
+''' letter = input()
 
 if letter.isalpha():
     print("ALPHA")
@@ -133,4 +133,114 @@ if letter.isalpha():
     if letter.isupper(): print("IS CAPITAL")
     else: print("IS SMALL")
     
-elif letter.isdigit(): print("IS DIGIT")
+elif letter.isdigit(): print("IS DIGIT") '''
+
+# ---------------------------
+
+# N. Char
+''' char = input()
+if char.isupper(): print(char.lower())
+else: print(char.upper()) '''
+
+# ---------------------------
+
+# O. Calculator
+
+''' expression = input()
+addition = expression.find("+")
+subtraction = expression.find("-")
+multiblication = expression.find("*")
+divition = expression.find("/")
+
+def get_nums(expression, sign):
+    a = int(expression[:sign])
+    b = int(expression[sign+1:])
+    return a,b
+
+if addition != -1:
+    a,b = get_nums(expression, addition)
+    print(a+b)
+elif subtraction != -1:
+    a,b = get_nums(expression, subtraction)
+    print(a-b)
+elif multiblication != -1:
+    a,b = get_nums(expression, multiblication)
+    print(a*b)
+elif divition != -1:
+    a,b = get_nums(expression, divition)
+    print(a//b) '''
+
+# ---------------------------
+
+# P. First digit !0
+''' x = int(input()[0])
+if x%2 == 0: print("EVEN")
+else: print("ODD") '''
+
+# ---------------------------
+
+# Q. Coordinates of a Point
+''' points = list(map(float, input().split()))
+x = points[0]
+y = points[1]
+
+if x>0 and y>0: 
+    print("Q1")
+elif x<0 and y>0: 
+    print("Q2")
+elif x<0 and y<0: 
+    print("Q3")
+elif x>0 and y<0: 
+    print("Q4")
+elif x==0 and y==0: 
+    print("Origem")
+elif y==0: 
+    print("Eixo X")
+elif x==0: 
+    print("Eixo Y") '''
+    
+# ---------------------------
+
+# R. Age in Days
+
+''' days = int(input())
+years = days // 365
+days -= (years * 365)
+months = days // 30
+days -= (months * 30)
+
+print(f"{years} years\n{months} months\n{days} days") '''
+
+# ---------------------------
+
+# S. Interval
+# [0,25], (25,50], (50,75], (75,100]
+
+''' x = float(input())
+if x>=0 and x <=100:
+    if x<=25: print("Interval [0,25]")
+    elif x<=50: print("Interval (25,50]")
+    elif x<=75: print("Interval (50,75]")
+    else: print("Interval (75,100]")
+else: print("Out of Intervals") '''
+
+# ---------------------------
+
+# T. Sort Numbers
+''' nums = list(map(int, input().split()))
+unsorted_nums = nums.copy()
+nums.sort()
+for i in nums: 
+    print(i)
+print("")
+for i in unsorted_nums: 
+    print(i) '''
+    
+# ---------------------------
+
+# U. Float or int
+''' num = float(input())
+int_part = int(num)
+float_part = num - float(int_part)
+if float_part == 0: print(f"int {int_part}")
+else: print("float %d %f" % (int_part, float_part)) '''
