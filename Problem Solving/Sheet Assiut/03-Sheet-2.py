3c1d5051ec74f37a8d8f1d255125e35afde72dfa
Abdullah Zaky <Abdullah.Zaky@intellisec-me.com>
2025-02-19 20:29:05 +0200

    added new files

--- Problem Solving/Sheet Assiut/03-Sheet-2.py
+++ Problem Solving/Sheet Assiut/03-Sheet-2.py
@@ -224,3 +224,188 @@ while n>0:
     print()
     n-=1 '''
 # -------------------------
+
+# R. Sequence of Numbers and Sum
+''' while True:
+    line = list(map(int, input().split()))
+    n1 = line[0]
+    n2 = line[1]
+    if n1<=0 or n2<=0:
+        break
+    sum = 0
+    for i in range(min(n1,n2), max(n1,n2)+1):
+        print(i, end=" ")
+        sum+=i
+    print(f"sum ={sum}") '''
+
+# -------------------------
+
+# S. Sum of Consecutive Odd Numbers
+''' n = int(input())
+while n:
+    line = list(map(int, input().split()))
+    n1 = line[0]
+    n2 = line[1] 
+    odd_sum = 0
+    if n1%2: odd_sum-=n1
+    if n2%2: odd_sum-=n2
+    for i in range(min(n1,n2), max(n1,n2)+1):
+        if i%2: 
+            odd_sum+=i
+    print(odd_sum)
+    n-=1 '''
+# -------------------------
+
+# T. Shape2
+''' n = int(input())
+i=1
+size = (n*2)-1
+pyramid = [[' ']*size]*n
+
+idx = 0
+l = size//2
+r = l+1
+while idx<n:
+    for i in range(l, r):
+        pyramid[idx][i] = '*'
+    for i in range(0, r):
+        print(pyramid[idx][i], end="")
+    print()
+    idx+=1
+    l-=1
+    r+=1 '''
+# -------------------------
+
+# U. Some Sums
+''' line = list(map(int, input().split()))
+n = line[0]
+a = line[1] 
+b = line[2] 
+
+def get_sum_of_digits(num):
+    sum = 0
+    while num:
+        sum+=num%10
+        num//=10
+    return sum
+
+total_sum = 0
+for i in range(1, n+1):
+    sod = get_sum_of_digits(i)
+    if(sod>= a and sod <= b): 
+        total_sum += i
+print(total_sum) '''
+# -------------------------
+
+# V. PUM
+''' n = int(input())
+i = 1
+while n:
+    c  = 3
+    while c:
+        print(i, end=" ")
+        i+=1
+        c-=1
+    print("PUM")
+    i+=1
+    n-=1 '''
+# -------------------------
+
+# W. Shape3 
+''' n = int(input())
+
+def draw_pyramid(n):
+    size = (n*2)-1
+    pyramid = [[' ' for _ in range(size)] for _ in range(n)]
+
+    idx = 0
+    l = size//2
+    r = l+1
+    while idx<n:
+        for i in range(l, r):
+            pyramid[idx][i] = '*'
+        idx+=1
+        l-=1
+        r+=1
+    return pyramid
+
+pyramid = draw_pyramid(n)
+reversed_pyramid = pyramid[::-1]
+
+def print_pyramid(pyramid, n, is_reversed=0):
+    if is_reversed : r = (n*2)-1
+    else: r=n
+    for i in range(len(pyramid)):
+        line = pyramid[i]
+        for j in range(0, r):
+            print(line[j], end="")
+        print()
+        if is_reversed : r-=1
+        else: r+=1
+
+
+print_pyramid(pyramid, n)
+print_pyramid(reversed_pyramid, n, 1) '''
+# -------------------------
+
+# X. Convert To Decimal 2
+''' n = int(input())
+while n:
+    number = int(input())
+    bin_rep = bin(number)
+    ones = ""
+    for c in str(bin_rep):
+        if c == "1":
+            ones+="1"
+    print(int(ones, 2))
+    n-=1 '''
+# -------------------------
+
+# Y. Easy Fibonacci
+
+''' n = int(input())
+f1 = 0
+f2 = 1
+print(f1, end=" ")
+
+if n>=2: 
+    print(f2, end=" ")
+    i=3
+    while i<=n:
+        f = f1+f2
+        print(f, end=" ")
+        f1 = f2
+        f2 = f
+        i+=1 '''
+# -------------------------
+
+# Z. Three Numbers -> wrong answer
+# basic solution: 3 nested loops o(n*3), but unfortunetly, its not me...
+# i wanna extract an equation for solving it in O(1).
+line = list(map(int, input().split()))
+k = line[0]
+s = line[1]
+counter = 0
+for i in range(0, k+1):
+    for j in range(0, k+1):
+        l = s-(i+j)
+        if l>=0 and l+i+j==s:
+            counter+=1
+               
+
+print(counter)
+
+''' if s<=k:
+    print(((s+1)*(s+2))//2)
+else:
+    m = s//3
+    if k < m: 
+        print(0)
+    elif k == m:
+        print(1) # its right for here
+ '''    ''' else:
+        k-=m
+        k+=1
+        print((k*6)-3) '''
+
+
